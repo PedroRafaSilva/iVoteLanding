1593cf1da0f0506e71d6367149b9094e6ecd0192
Pedro Silva <pedrosedry@gmail.com>
2023-11-20 12:25:13 +0000

    Added: Favicon

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+	<link rel="icon" href="images/logo/favicon.icon" type="images/x-icon">
 
 	<!-- Core Style -->
 	<link rel="stylesheet" href="css/style.css">
